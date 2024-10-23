f4c3cbee5412ef634f94d257a37909047b97cf7a
krister <krister@trangius.se>
2024-10-23 10:07:07 +0200

    make a nice menu

--- Program.cs
+++ Program.cs
@@ -2,6 +2,33 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Menu");
+        Console.WriteLine("1. Add");
+        Console.WriteLine("2. Subtract");
+        Console.WriteLine("3. Multiply");
+        Console.WriteLine("4. Divide");
+        Console.WriteLine("5. Exit");
+        Console.Write("Enter your choice: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                //Add();
+                break;
+            case 2:
+                //Subtract();
+                break;
+            case 3:
+                //Multiply();
+                break;
+            case 4:
+                //Divide();
+                break;
+            case 5:
+                return;
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
+        }
     }
 }
